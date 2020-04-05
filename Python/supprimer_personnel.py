75974e7e699af9e23e0f3afcd04ee10b80ae4153
Matteo Ledoux <matteo.ledoux@etu.utc.fr>
2020-04-05 12:09:00 +0200

    Update supprimer_personnel.py

--- Python/supprimer_personnel.py
+++ Python/supprimer_personnel.py
@@ -1,12 +1,16 @@
+from ajout_personnel import *
+
 def supprimer_personnel(HOST, DATABASE, USER, PASSWORD):
 
 	import psycopg2
-    	
+
 	conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (HOST, DATABASE, USER, PASSWORD))
 	cur = conn.cursor()
 
+	afficher_personnel(HOST, DATABASE, USER, PASSWORD)
+
+	print ("\n")
 	sup = int(input("Quel est l'identifiant de la personne que vous souhaitez supprimer?"))
-	
 
 	sql = "DELETE FROM Spécialité WHERE personnel = %i ;"%(sup);
 	cur.execute(sql)
