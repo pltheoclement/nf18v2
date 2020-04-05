7c22c0799fdd38b819615c271701f9f5990b6866
Matteo Ledoux <matteo.ledoux@etu.utc.fr>
2020-04-05 12:11:20 +0200

    Update saisir_taille.py

--- Python/saisir_taille.py
+++ Python/saisir_taille.py
@@ -41,9 +41,9 @@ def saisir_taille(HOST, DATABASE, USER, PASSWORD):
     erreur = False
     while (not erreur):
         try:
-            JSaisie = int(input("    Jour : "))
-            MSaisie = int(input("    Mois : "))
-            ASaisie = int(input("    Année : "))
+            JSaisie = int(input("    Jour (JJ) : "))
+            MSaisie = int(input("    Mois (MM) : "))
+            ASaisie = int(input("    Année (AAAA) : "))
             erreur = True
         except:
             print ("Erreur, veuillez entrer des entiers positifs")
